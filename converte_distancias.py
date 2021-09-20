da5ed398d891fedb9fbebb73a145c4739826275c
Alessandro de Cristo Moleta <90357593+alemoleta@users.noreply.github.com>
2021-09-20 02:42:20 -0300

    Create converte_distancias.py
    Converte:
    . milhas para quilómetros;
    . quilómetros para milhas.

---
+++ converte_distancias.py
@@ -0,0 +1,11 @@
+# dados de exemplo para variáveis a converter
+kilometers = 12.25 
+miles = 7.38 
+
+# cálculos das conversões
+miles_to_kilometers = miles * 1.61
+kilometers_to_miles = kilometers / 1.61
+
+# apresentação dos resultados arredondados
+print(miles, "miles is", round(miles_to_kilometers, 2), "kilometers")
+print(kilometers, "kilometers is", round(kilometers_to_miles, 2), "miles")

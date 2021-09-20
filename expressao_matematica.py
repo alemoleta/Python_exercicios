a5b04d7ebd2e3666169818cda826e786d2e1a058
Alessandro de Cristo Moleta <90357593+alemoleta@users.noreply.github.com>
2021-09-20 03:10:38 -0300

    Create expressao_matematica.py
    Código para realizar expressão matemática solicitada em python.

---
+++ expressao_matematica.py
@@ -0,0 +1,11 @@
+# hardcode your test data here
+x = -1 
+
+# transforma em float
+x = float(x) 
+
+# expressão matemática
+y = (3 * (x ** 3) - 2 * (x ** 2) + 3 * x - 1)
+
+# mostra o resultado
+print("y =", y)

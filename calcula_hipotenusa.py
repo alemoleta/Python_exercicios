7dd3626b49c706bac8829f764858c1f0cf0d8490
Alessandro de Cristo Moleta <90357593+alemoleta@users.noreply.github.com>
2021-09-26 19:46:38 -0300

    Create calcula_hipotenusa.py

---
+++ calcula_hipotenusa.py
@@ -0,0 +1,4 @@
+#Programa que calcula o valor da hipotenusa
+cat_a = float(input("Digite o valor do cateto a: "))
+cat_b = float(input("Digite o valor do cateto b: "))
+print("O valor da hipotenusa é: ", (cat_a**2 + cat_b**2) ** .5)

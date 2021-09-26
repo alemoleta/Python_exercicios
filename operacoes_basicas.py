3b6ba4448af4ac6ec9a79c4ef875fae63ef342e5
Alessandro de Cristo Moleta <90357593+alemoleta@users.noreply.github.com>
2021-09-26 20:13:15 -0300

    Create operacoes_basicas.py

---
+++ operacoes_basicas.py
@@ -0,0 +1,11 @@
+#Programa que recebe 2 valores, converte para float e mostra resultados de operações matemáticas básicas
+
+var_a = float(input("Digite o valor a: "))# input a float value for variable a here
+var_b = float(input("Digite o valor b: "))# input a float value for variable b here
+
+print("a + b = ", var_a + var_b)# output the result of addition here
+print("a - b = ", var_a - var_b)# output the result of subtraction here
+print("a x b = ", var_a * var_b)# output the result of multiplication here
+print("a / b = ", var_a / var_b)# output the result of division here
+
+print("\nÉ isso!")

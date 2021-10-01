004e8ffc71fbdb2aaeca5ef1c9df903ad65ba8d9
Alessandro de Cristo Moleta <90357593+alemoleta@users.noreply.github.com>
2021-10-01 09:02:08 -0300

    Create Operador_compara.py

---
+++ Operador_compara.py
@@ -0,0 +1,12 @@
+# Curso Python Cisco e Python Institute
+# 3.1.1.4 LAB
+# familiarizar-se com a função input() ;
+# familiarizar-se com os operadores de comparação em Python.
+
+# Cenário:
+# Usando um dos operadores de comparação em Python, escreva um programa simples de duas linhas que toma o parâmetro n como input, 
+# que é um inteiro, e imprime False Se n for menor que 100, e True Se n for maior ou igual que 100.
+# Não crie blocos if nenhuns (vamos falar deles muito em breve). Teste o seu código utilizando os dados que lhe fornecemos.
+
+n = int(input("Digite um valor: "))
+print (n >= 100)
